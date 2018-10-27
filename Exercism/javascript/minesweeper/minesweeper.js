19c89efc73fcce0e332fed9c9df67ae2052cf273
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-10-27 15:32:33 +0200

    moved method out of class

--- Exercism/javascript/minesweeper/minesweeper.js
+++ Exercism/javascript/minesweeper/minesweeper.js
@@ -2,10 +2,38 @@
 const bombsOnRow = (row, idx) => {
   let bombs = 0;
   if (row.length >= idx - 1) {
-    bombs += row[idx - 1] === '*' ? 1 : 0;
-    bombs += row[idx] === '*' ? 1 : 0;
-    bombs += row[idx + 1] === '*' ? 1 : 0;
+    bombs += row[idx - 1] === '*' ? 1 : 0; // left
+    bombs += row[idx] === '*' ? 1 : 0; // above
+    bombs += row[idx + 1] === '*' ? 1 : 0; // right
   }
+  return bombs;
+};
+
+const adjacentBombs = (board, rowIdx, row, colIdx) => {
+  let bombs = 0;
+
+  // ABOVE ROW
+  if (rowIdx > 0) {
+    const upperRow = board[rowIdx - 1];
+    bombs += bombsOnRow(upperRow, colIdx);
+  }
+
+  // OWN ROW
+  bombs += bombsOnRow(row, colIdx);
+
+  // BELOW ROW
+  if (rowIdx < board.length - 1) {
+    const lowerRow = board[rowIdx + 1];
+    bombs += bombsOnRow(lowerRow, colIdx);
+  }
+
+  if (row[colIdx] === '*') {
+    return '*';
+  }
+  if (bombs === 0) {
+    return ' ';
+  }
+
   return bombs;
 };
 
@@ -15,38 +43,8 @@ export class Minesweeper {
     if (!hasMines) return board;
 
     return board.map((row, rowIdx) => [...row]
-      .map((col, colIdx) => (col === ' ' ? this.adjacentBombs(board, rowIdx, row, colIdx) : col))
+      .map((col, colIdx) => (col === ' ' ? adjacentBombs(board, rowIdx, row, colIdx) : col))
       .join(''));
-  }
-
-  adjacentBombs(board, rowIdx, row, colIdx) {
-    this.bombs = 0;
-
-    // ABOVE ROW
-    if (rowIdx > 0) {
-      const upperRow = board[rowIdx - 1];
-      this.bombs += bombsOnRow(upperRow, colIdx);
-    }
-
-    // OWN ROW
-	  this.bombs += bombsOnRow(row, colIdx);
-
-
-    // BELOW ROW
-    if (rowIdx < board.length - 1) {
-      const lowerRow = board[rowIdx + 1];
-	  this.bombs += bombsOnRow(lowerRow, colIdx);
-    }
-
-    if (row[colIdx] === '*') {
-      return '*';
-    } 
-    
-    if (this.bombs === 0) {
-      return ' ';
-    }
-
-    return this.bombs;
   }
 }
 
