fe870353dbe657a8e762bcace66a5fa377a56e99
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-10-28 20:12:19 +0100

    fixed

--- Exercism/javascript/raindrops/raindrops.js
+++ Exercism/javascript/raindrops/raindrops.js
@@ -1,24 +1,9 @@
 export class Raindrops {
   static convert(n) {
-    const factors = [];
-    for (let i = 3; i <= 7; i += 2) {
-      if (n % i === 0) {
-        factors.push(i);
-      }
-    }
-    let res = '';
-    if (factors.includes(3)) {
-      res += 'Pling';
-    }
+    const pling = (n % 3) ? '' : 'Pling';
+    const plang = (n % 5) ? '' : 'Plang';
+    const plong = (n % 7) ? '' : 'Plong';
 
-    if (factors.includes(5)) {
-      res += 'Plang';
-    }
-
-    if (factors.includes(7)) {
-      res += 'Plong';
-    }
-
-    return res || n.toString();
+    return pling + plang + plong || n.toString();
   }
 }
