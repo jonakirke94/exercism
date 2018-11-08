acc6eec3574e48925e864ef1af78059bc963870a
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-11-08 19:17:27 +0100

    linted

--- Exercism/javascript/pangram/pangram.js
+++ Exercism/javascript/pangram/pangram.js
@@ -1,14 +1,15 @@
 const toLower = sentence => sentence.split('').map(x => x.toLowerCase()).join('');
 
-export const isPangram = sentence => {
-	const uniqueChars = [];
+export const isPangram = (sentence) => {
+  const uniqueChars = [];
 
-	toLower(sentence).split('')
-		.forEach(char => {
-			if (!uniqueChars.includes(char) && char.match(/[a-z]/i)) {
-				uniqueChars.push(char);
-			}
-	});
-	
-	return uniqueChars.length === 26;
-}
+  toLower(sentence)
+    .split('')
+    .forEach((char) => {
+      if (!uniqueChars.includes(char) && char.match(/[a-z]/i)) {
+        uniqueChars.push(char);
+      }
+    });
+
+  return uniqueChars.length === 26;
+};
