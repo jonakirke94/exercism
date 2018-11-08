5eed334afc588db20fff395c8720ae6916649d59
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-11-08 19:14:51 +0100

    done

---
+++ Exercism/javascript/pangram/pangram.js
@@ -0,0 +1,14 @@
+const toLower = sentence => sentence.split('').map(x => x.toLowerCase()).join('');
+
+export const isPangram = sentence => {
+	const uniqueChars = [];
+
+	toLower(sentence).split('')
+		.forEach(char => {
+			if (!uniqueChars.includes(char) && char.match(/[a-z]/i)) {
+				uniqueChars.push(char);
+			}
+	});
+	
+	return uniqueChars.length === 26;
+}
