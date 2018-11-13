2d5135219d0acad0c7c9a9baf2a2143ec19dbbf0
Jonathan <jonakirke94@gmail.com>
2018-11-13 22:08:20 +0100

    removed one ifs

--- Exercism/javascript/bob/bob.js
+++ Exercism/javascript/bob/bob.js
@@ -5,10 +5,21 @@ const isYelling = message => message === message.toUpperCase() && message.match(
 export const hey = (message) => {
   const msg = message.trim();
 
-  if (!msg) return 'Fine. Be that way!';
-  if (isAsking(msg) && isYelling(msg)) return 'Calm down, I know what I\'m doing!';
-  if (isAsking(msg)) return 'Sure.';
-  if (isYelling(msg)) return 'Whoa, chill out!';
+  if (!msg) {
+    return 'Fine. Be that way!';
+  }
+
+  if (isAsking(msg) && isYelling(msg)) {
+    return 'Calm down, I know what I\'m doing!';
+  }
+
+  if (isAsking(msg)) {
+    return 'Sure.';
+  }
+  
+  if (isYelling(msg)) {
+    return 'Whoa, chill out!';
+  }
 
   return 'Whatever.';
 };
