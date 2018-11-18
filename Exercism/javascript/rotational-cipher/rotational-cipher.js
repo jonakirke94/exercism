244e1ba33bd6db688a3028d39b3ddce443496c08
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-11-18 21:36:02 +0100

    done

---
+++ Exercism/javascript/rotational-cipher/rotational-cipher.js
@@ -0,0 +1,23 @@
+export class RotationalCipher {
+	static rotate(plain, key) {
+
+		return plain
+			.split('')
+			.map(x => {
+				if (!x.match(/[a-zA-z]/)) return x;
+
+				let code = x.charCodeAt();
+
+				// small etter 
+				if (code >= 97 && code <= 122) {
+					code = code + key > 122 ? 96 + ((code + key) % 122) : code + key;
+				} else {
+					console.log('was capital');
+					code = code + key > 90 ? 64 + ((code + key) % 90) : code + key;
+				}
+
+				return String.fromCharCode(code);
+			})
+			.join('');
+	}
+}
