68621b92a23603c7f16049dae1db6cd1052f2748
Jonathan <jonakirke94@gmail.com>
2018-11-20 21:06:30 +0100

    linted

--- Exercism/javascript/acronym/acronym.js
+++ Exercism/javascript/acronym/acronym.js
@@ -1,10 +1,10 @@
-export const parse = sentence => {
-    // handle colon
-    const colonArr = sentence.split(':');
-    if (colonArr.length > 1) return colonArr[0];
+export const parse = (sentence) => {
+  // handle colon
+  const colonArr = sentence.split(':');
+  if (colonArr.length > 1) return colonArr[0];
 
-    return sentence
+  return sentence
     .split(/\s|-|(?=[A-Z])/g)
     .map(word => word.charAt(0).toUpperCase())
     .join('');
-}
+};
