568ac4e8b5270b3fd1d620d358e7e6305022f437
Jonathan <jonakirke94@gmail.com>
2018-11-20 22:35:19 +0100

    optimized

--- Exercism/javascript/high-scores/high-scores.js
+++ Exercism/javascript/high-scores/high-scores.js
@@ -1,21 +1,16 @@
 export class HighScores {
   constructor(input) {
-    this.build(input);
+    this.scores = input;
+    this.latest = input[input.length - 1];
+    this.top = [...input].sort((a, b) => b - a).slice(0, 3);
+    this.highest = Math.max(...input);
+    this.report = this.buildReport();
   }
 
-  build(input) {
-    const sorted = Array.from(input).sort((a, b) => b - a);
-    const [highest] = sorted; // destructuring array
-    this.scores = Array.from(input);
-    this.latest = input[input.length - 1];
-    this.highest = highest;
-    this.top = sorted.slice(0, 3);
-
-    // report
-    const diff = this.highest - this.latest;
-    const newBest = 'That\'s your personal best!';
-    const oldBest = `That's ${diff} short of your personal best!`;
+  buildReport() {
+    const newBest = 'your personal best!';
+    const oldBest = `${this.highest - this.latest} short of your personal best!`;
     const msg = this.latest >= this.highest ? newBest : oldBest;
-    this.report = `Your latest score was ${this.latest}. ${msg}`;
+    return `Your latest score was ${this.latest}. That's ${msg}`;
   }
 }
