1184785b818eaa3c63fe150cb93d18c2b0f14f1c
Jonathan <jonakirke94@gmail.com>
2018-11-20 21:15:26 +0100

    onelined

--- Exercism/javascript/acronym/acronym.js
+++ Exercism/javascript/acronym/acronym.js
@@ -1,10 +1,5 @@
-export const parse = (sentence) => {
-  // handle colon
-  const colonArr = sentence.split(':');
-  if (colonArr.length > 1) return colonArr[0];
-
-  return sentence
-    .split(/\s|-|(?=[A-Z])/g)
-    .map(word => word.charAt(0).toUpperCase())
-    .join('');
-};
+export const parse = sentence => sentence
+  .split(':')[0]
+  .split(/\s|-|(?=[A-Z])/g)
+  .map(word => word.charAt(0).toUpperCase())
+  .join('');
