3085244f5de944481cf5df4538dfd57ba4bc8cff
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-11-25 19:06:31 +0100

    done

--- Exercism/javascript/word-count/word-count.js
+++ Exercism/javascript/word-count/word-count.js
@@ -1,16 +1,16 @@
 export class Words {
+  // eslint-disable-next-line class-methods-use-this
+  count(words) {
+    const res = {};
+    const normalized = words.replace(/\n|\t|\s+/g, ' ');
+    normalized
+      .toLowerCase()
+      .trim()
+      .split(' ')
+      .forEach(
+        (word, _index, arr) => (res[word] = arr.filter(x => x === word).length),
+      );
 
-	count(words) {
-		const res = {};
-		const normalized = words.replace(/\n|\t|\s+/g, ' ');
-		normalized
-			.toLowerCase()
-			.trim()
-			.split(' ')
-		  .forEach((word, _index, arr) => res[word] = (arr.filter(x => x === word)).length);
-
-		return res;
-	}
-
+    return res;
+  }
 }
-
