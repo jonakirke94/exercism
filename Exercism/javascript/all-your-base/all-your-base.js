d66eb3c6b52f48c565d57a8c094fbf4505d4149d
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2018-11-27 18:06:44 +0100

    done

--- Exercism/javascript/all-your-base/all-your-base.js
+++ Exercism/javascript/all-your-base/all-your-base.js
@@ -1,16 +1,32 @@
-const error = () => {
-  throw Error('Input has wrong format');
+const validBase = base => !(!base || base < 2 || !Number.isInteger(base));
+
+const validNumbers = (numbers, from) => {
+  const leadingZeros = numbers.length > 1 && numbers[0] === 0;
+  const invalidNums = numbers.length === 0 || numbers.filter(n => n < 0 || n >= from).length;
+  return !leadingZeros && !invalidNums;
 };
 
 export class Converter {
+  // eslint-disable-next-line class-methods-use-this
   convert(numbers, from, to) {
-    let calculation = 0;
-    numbers.forEach((n, idx) => {
+    if (!validBase(from)) {
+      throw new Error('Wrong input base');
+    }
 
-    });
+    if (!validBase(to)) {
+      throw new Error('Wrong output base');
+    }
 
+    if (!validNumbers(numbers, from)) {
+      throw new Error('Input has wrong format');
+    }
 
+    let convNum = numbers.reduce((pre, cur) => pre * from + cur, 0);
+    const res = [];
+    do {
+      res.push(convNum % to);
+      convNum = Math.floor(convNum / to);
+    } while (convNum > 0);
+    return res.reverse();
   }
 }
-
-// the right most digit in FROM is the remainder of the division of n byb2
